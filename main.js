09cb72fed3f93503919b7c3d768aeede66d37f95
JahanaSultan <cahana.aliyeva@gmail.com>
2022-06-23 18:29:08 +0400

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+let city = document.querySelector('.city')
+let temp = document.querySelector('.temp')
+let descr=document.querySelector('.descr')
+let minmax=document.querySelector('.minmax')
+let humidity=document.querySelector('.humidity')
+let pressure=document.querySelector('.pressure')
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Baku&appid=cc7c8fdfcbbccc9dc62e21bde4bb83ee&units=metric&lang=az")
+.then(res=>res.json())
+.then(data=>{
+  city.innerText = `${data.name},${data.sys.country}`,
+  temp.innerText=`${Math.round(data.main.temp)}°C`,
+  descr.innerText= data.weather[0].description,
+  minmax.innerText=`${Math.round(data.main.temp_min)}°C / ${Math.round(data.main.temp_max)}°C`,
+  pressure.innerText=`Atmosfer Təziqi: ${data.main.pressure} hpa`,
+  humidity.innerText=`Nəmlik: ${data.main.humidity}%`
+})
+
+
 let url = 'https://api.openweathermap.org/data/2.5/'
 let apikey = 'cc7c8fdfcbbccc9dc62e21bde4bb83ee'
 
@@ -13,27 +33,19 @@ let getResult = (city) => {
     })
     .then(result)
 }
+
+
+
 let result = (weather) => {
-  let city = document.querySelector('.city')
   city.innerText = `${weather.name},${weather.sys.country}`
-
-  let temp = document.querySelector('.temp')
   temp.innerText=`${Math.round(weather.main.temp)}°C`
-
-  let descr=document.querySelector('.descr')
   descr.innerText= weather.weather[0].description
-
-  let minmax=document.querySelector('.minmax')
   minmax.innerText=`${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`
-
-  let pressure=document.querySelector('.pressure')
   pressure.innerText=`Atmosfer Təziqi: ${weather.main.pressure} hpa`
-
-  let humidity=document.querySelector('.humidity')
   humidity.innerText=`Nəmlik: ${weather.main.humidity}%`
-  console.log(weather)
   input.value=''
 }
+
 
 let input = document.querySelector('#input')
 
